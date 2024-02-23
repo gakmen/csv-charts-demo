9c98a901fb8c673b334d2bc9aeb571254216ad37
Gosha Akmen <gakmen@icloud.com>
2024-02-23 20:29:11 +0500

    Подготовил тестовую среду

--- csv-charts-demo/ChartModel.swift
+++ csv-charts-demo/ChartModel.swift
@@ -5,8 +5,6 @@
 //  Created by  Gosha Akmen on 23.02.2024.
 //
 
-import Foundation
-
 struct ChartModel {
     let x: [Date]
     let y: [Int]
---
+++ csv-charts-demoTests/ChartLoaderTests.swift
@@ -0,0 +1,15 @@
+//
+//  ChartLoaderTests.swift
+//  csv-charts-demoTests
+//
+//  Created by  Gosha Akmen on 23.02.2024.
+//
+
+import XCTest
+
+class ChartLoaderTests: XCTestCase {
+    
+    func test() {
+        
+    }
+}
