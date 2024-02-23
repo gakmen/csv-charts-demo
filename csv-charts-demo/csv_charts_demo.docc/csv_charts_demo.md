70592ba498e95f30ed885d96017701f727255e38
Gosha Akmen <gakmen@icloud.com>
2024-02-23 17:42:05 +0500

    Initial Commit

---
+++ csv-charts-demo/csv_charts_demo.docc/csv_charts_demo.md
@@ -0,0 +1,13 @@
+# ``csv_charts_demo``
+
+<!--@START_MENU_TOKEN@-->Summary<!--@END_MENU_TOKEN@-->
+
+## Overview
+
+<!--@START_MENU_TOKEN@-->Text<!--@END_MENU_TOKEN@-->
+
+## Topics
+
+### <!--@START_MENU_TOKEN@-->Group<!--@END_MENU_TOKEN@-->
+
+- <!--@START_MENU_TOKEN@-->``Symbol``<!--@END_MENU_TOKEN@-->
---
+++ csv-charts-demo/csv_charts_demo.h
@@ -0,0 +1,18 @@
+//
+//  csv_charts_demo.h
+//  csv-charts-demo
+//
+//  Created by  Gosha Akmen on 23.02.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for csv_charts_demo.
+FOUNDATION_EXPORT double csv_charts_demoVersionNumber;
+
+//! Project version string for csv_charts_demo.
+FOUNDATION_EXPORT const unsigned char csv_charts_demoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <csv_charts_demo/PublicHeader.h>
+
+
