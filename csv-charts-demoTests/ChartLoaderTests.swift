ed804e2855575969ab63e3a400b400f74a8b2b71
Gosha Akmen <gakmen@icloud.com>
2024-02-24 07:32:26 +0500

    Рефакторинг

--- csv-charts-demoTests/ChartLoaderTests.swift
+++ csv-charts-demoTests/ChartLoaderTests.swift
@@ -15,7 +15,7 @@ class ChartLoader {
     let client: LocalClient
     let url: URL
     
-    init(client: LocalClient, url: URL = URL(string: "file:///path/to/placeholderFile.csv")!) {
+    init(client: LocalClient, url: URL) {
         self.client = client
         self.url = url
     }
@@ -25,30 +25,37 @@ class ChartLoader {
     }
 }
 
-class LocalClientSpy: LocalClient {
-    var requestedURL: URL?
-    
-    func get(from url: URL) {
-        requestedURL = url
-    }
-}
-
 class ChartLoaderTests: XCTestCase {
     
     func test_init_doesNotRequestDataFromURL() {
-        let client = LocalClientSpy()
-        let sut = ChartLoader(client: client)
+        let (sut, client) = makeSUT()
         
         XCTAssertNil(client.requestedURL)
     }
     
     func test_load_requestsDataFromURL() {
         let url = URL(string: "file:///path/to/someFile.csv")!
-        let client = LocalClientSpy()
-        let sut = ChartLoader(client: client, url: url)
+        let (sut, client) = makeSUT(with: url)
         
         sut.load()
         
         XCTAssertEqual(client.requestedURL, url)
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(with url: URL = URL(string: "file:///path/to/placeholderFile.csv")!) -> (ChartLoader, LocalClientSpy) {
+        let client = LocalClientSpy()
+        let sut = ChartLoader(client: client, url: url)
+        
+        return (sut, client)
+    }
+    
+    private class LocalClientSpy: LocalClient {
+        var requestedURL: URL?
+        
+        func get(from url: URL) {
+            requestedURL = url
+        }
     }
 }
