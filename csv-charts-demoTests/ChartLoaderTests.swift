98c94383e42489f0eb265b4754a513fb09a3bb0c
Gosha Akmen <gakmen@icloud.com>
2024-02-24 06:59:09 +0500

    Вызов `load` запрашивает данные по URL

--- csv-charts-demoTests/ChartLoaderTests.swift
+++ csv-charts-demoTests/ChartLoaderTests.swift
@@ -7,21 +7,48 @@
 
 import XCTest
 
-class ChartLoader {
-    
+protocol LocalClient {
+    func get(from url: URL)
 }
 
-struct LocalStoreSpy {
+class ChartLoader {
+    let client: LocalClient
+    let url: URL
+    
+    init(client: LocalClient, url: URL = URL(string: "file:///path/to/placeholderFile.csv")!) {
+        self.client = client
+        self.url = url
+    }
+    
+    func load() {
+        client.get(from: url)
+    }
+}
+
+class LocalClientSpy: LocalClient {
     var requestedURL: URL?
+    
+    func get(from url: URL) {
+        requestedURL = url
+    }
 }
 
 class ChartLoaderTests: XCTestCase {
     
     func test_init_doesNotRequestDataFromURL() {
-        let store = LocalStoreSpy()
-        let sut = ChartLoader()
+        let client = LocalClientSpy()
+        let sut = ChartLoader(client: client)
         
-        XCTAssertNil(store.requestedURL)
+        XCTAssertNil(client.requestedURL)
     }
     
+    func test_load_requestsDataFromURL() {
+        let url = URL(string: "file:///path/to/someFile.csv")!
+        let client = LocalClientSpy()
+        let sut = ChartLoader(client: client, url: url)
+        
+        sut.load()
+        
+        XCTAssertEqual(client.requestedURL, url)
+    }
 }
